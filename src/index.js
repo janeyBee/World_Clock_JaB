6d33d85750d5fc6a159eeb8d5d8298c234e4c654
janeyBee <janebredius@gmail.com>
2024-11-15 16:39:01 +0000

    Added JavaScript, integrated Moment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+function updateTime() {
+  let visbyElement = document.querySelector('#visby');
+  let visbyDateElement = visbyElement.querySelector(' .date');
+  let visbyTimeElement = visbyElement.querySelector(' .time');
+  let visbyTime = moment().tz('Europe/Stockholm');
+  visbyDateElement.innerHTML = `${visbyTime.format('dddd MMMM Do YYYY')}`;
+  visbyTimeElement.innerHTML = `${visbyTime.format('h:mm:ss')}
+<small>${visbyTime.format('A')}</small>`;
 
+  let santiagoElement = document.querySelector('#santiago');
+  let santiagoDateElement = santiagoElement.querySelector(' .date');
+  let santiagoTimeElement = santiagoElement.querySelector(' .time');
+  let santiagoTime = moment().tz('America/Santiago');
+  santiagoDateElement.innerHTML = `${santiagoTime.format('dddd MMMM Do YYYY')}`;
+  santiagoTimeElement.innerHTML = `${santiagoTime.format('h:mm:ss')}
+<small>${santiagoTime.format('A')}</small>`;
+
+  let serpinsElement = document.querySelector('#serpins');
+  let serpinsDateElement = serpinsElement.querySelector(' .date');
+  let serpinsTimeElement = serpinsElement.querySelector(' .time');
+  let serpinsTime = moment().tz('Europe/Lisbon');
+  serpinsDateElement.innerHTML = `${serpinsTime.format('dddd MMMM Do YYYY')}`;
+  serpinsTimeElement.innerHTML = `${serpinsTime.format('h:mm:ss')}
+<small>${serpinsTime.format('A')}</small>`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
